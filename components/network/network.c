454a16e659f920b7b219bae235d72281e3cca926
pablo.haller.dev@gmail.com <pablo.haller.dev@gmail.com>
2024-06-29 16:37:30 -0300

    Proyect init

---
+++ components/network/network.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "network.h"
+#include "esp_log.h"
+
+#define TAG "network"
+void init_network(void)
+{
+    ESP_LOGI(TAG, "Network initialized!");
+    ESP_LOGI(TAG, "Network initialized 2!");
+    ESP_LOGI(TAG, "Network initialized 3!");
+}
