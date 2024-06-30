2052f1df75b39682dc0f574aaf456c16d44e8c73
pablo.haller.dev@gmail.com <pablo.haller.dev@gmail.com>
2024-06-29 21:38:16 -0300

    feat: working apsta

--- components/network/network.c
+++ components/network/network.c
@@ -18,21 +18,21 @@
 
 static EventGroupHandle_t wifi_event_group;
 const int CONNECTED_BIT = BIT0;
+const int AP_STARTED_BIT = BIT1;
 
 static void sta_event_handler(void *arg, esp_event_base_t event_base,
                               int32_t event_id, void *event_data)
 {
-    ESP_LOGI(TAG, "Enter!");
     static int s_retry_num = 0;
+    const int max_retry = 10;
 
     if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
     {
-        ESP_LOGI(TAG, "First connect!");
         esp_wifi_connect();
     }
     else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
     {
-        if (s_retry_num < 10)
+        if (s_retry_num < max_retry)
         {
             esp_wifi_connect();
             s_retry_num++;
@@ -50,56 +50,6 @@ static void sta_event_handler(void *arg, esp_event_base_t event_base,
         ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
         s_retry_num = 0;
         xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
-    }
-}
-
-void wifi_init_sta(void)
-{
-    wifi_event_group = xEventGroupCreate(); // Crear el grupo de eventos
-
-    ESP_LOGI(TAG, "WIFI_STA_INIT .... ... ... ");
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &sta_event_handler,
-                                                        NULL,
-                                                        &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-                                                        IP_EVENT_STA_GOT_IP,
-                                                        &sta_event_handler,
-                                                        NULL,
-                                                        &instance_got_ip));
-
-    wifi_config_t wifi_config = {
-        .sta = {
-            .ssid = "PABLO",
-            .password = "12345678",
-        },
-    };
-
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-    ESP_ERROR_CHECK(esp_wifi_connect());
-
-    EventBits_t bits = xEventGroupWaitBits(wifi_event_group,
-                                           CONNECTED_BIT,
-                                           pdFALSE,
-                                           pdTRUE,
-                                           pdMS_TO_TICKS(10000)); // Esperar 10 segundos
-    ESP_LOGI(TAG, "bits=%x", bits);
-
-    if (bits & CONNECTED_BIT)
-    {
-        ESP_LOGI(TAG, "WIFI_MODE_STA connected.");
-    }
-    else
-    {
-        ESP_LOGI(TAG, "WIFI_MODE_STA can't connected.");
     }
 }
 
@@ -140,18 +90,64 @@ void wifi_init_softap(void)
             .channel = EXAMPLE_ESP_WIFI_CHANNEL,
             .password = EXAMPLE_ESP_WIFI_PASS,
             .max_connection = EXAMPLE_MAX_STA_CONN,
-            .authmode = 0,
-        },
+            .authmode = 0},
     };
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
     ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
     ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI(TAG, "wifi_init_softap finished. SSID:%s password:%s channel:%d",
              EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS, EXAMPLE_ESP_WIFI_CHANNEL);
 
+    xEventGroupSetBits(wifi_event_group, AP_STARTED_BIT);
     init_webserver();
+}
+
+void wifi_init_sta(void)
+{
+    xEventGroupWaitBits(wifi_event_group, AP_STARTED_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+    esp_netif_create_default_wifi_sta();
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &sta_event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &sta_event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = "PABLO",
+            .password = "12345678",
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_connect());
+
+    EventBits_t bits = xEventGroupWaitBits(wifi_event_group,
+                                           CONNECTED_BIT,
+                                           pdFALSE,
+                                           pdTRUE,
+                                           pdMS_TO_TICKS(10000)); // Esperar 10 segundos
+    ESP_LOGI(TAG, "bits=%x", bits);
+
+    if (bits & CONNECTED_BIT)
+    {
+        ESP_LOGI(TAG, "WIFI_MODE_STA connected.");
+    }
+    else
+    {
+        ESP_LOGI(TAG, "WIFI_MODE_STA can't connect.");
+    }
 }
 
 void init_network(void)
